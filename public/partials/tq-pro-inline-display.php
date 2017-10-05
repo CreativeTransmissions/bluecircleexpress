f33cde67984f8f42680eb0bef4543249a5daeb44
Andrew van Duivenbode <andrew@creativetransmissions.com>
2017-10-05 12:25:22 +0100

    remove postcode

--- public/partials/tq-pro-inline-display.php
+++ public/partials/tq-pro-inline-display.php
@@ -29,12 +29,12 @@
                             <input tabindex="10" class="text addresspicker" required type="text" name="address_0_address" id="address_0" value="" autocomplete="false"/>
                             <span class="bt-flabels__error-desc">Required: Collection Address</span>
                         </div>
-                        <div class="inline-block bt-flabels__wrapper half">
+               <!--         <div class="inline-block bt-flabels__wrapper half">
                             <input class="inline-block half-field" type="text" id="address_0_appartment_no" name="address_0_appartment_no" placeholder="Unit" value=""/>
                         </div>
                         <div class="inline-block bt-flabels__wrapper half right last-address-field">
                             <input class="inline-block postcode half-field half-field-right" type="text" id="address_0_postal_code" name="address_0_postal_code" placeholder="Postcode" value=""/>
-                        </div>
+                        </div>-->
                     </div>
                     <div class="address-wrap">
                         <div class="field bt-flabels__wrapper full-width">
@@ -43,12 +43,12 @@
                             <input  tabindex="11" class="text addresspicker" required type="text" name="address_1_address" id="address_1" value="" autocomplete="false"/>
                             <span class="bt-flabels__error-desc">Required: Destination Address</span>
                         </div>
-                        <div class="inline-block bt-flabels__wrapper half">
+                <!--        <div class="inline-block bt-flabels__wrapper half">
                             <input class="inline-block half-field " type="text" id="address_1_appartment_no" name="address_1_appartment_no" placeholder="Unit" value=""/>
                         </div>
                         <div class="inline-block bt-flabels__wrapper half right last-address-field">
                             <input class="inline-block postcode half-field half-field-right" type="text" id="address_1_postal_code" name="address_1_postal_code" placeholder="Postcode" value=""/>
-                        </div>
+                        </div> -->
                     </div>
                 <?php } else { ?>
                     <legend>Delivery Addresses</legend>
@@ -59,12 +59,12 @@
                             <input tabindex="10" class="text addresspicker" required type="text" name="address_1_address" id="address_1" value="" autocomplete="false"/>
                             <span class="bt-flabels__error-desc">Required: Destination Address</span>
                         </div>
-                        <div class="inline-block bt-flabels__wrapper half">
+               <!--          <div class="inline-block bt-flabels__wrapper half">
                             <input class="inline-block half-field" type="text" id="address_1_appartment_no" name="address_1_appartment_no" placeholder="Unit" value=""/>
                         </div>
                         <div class="inline-block bt-flabels__wrapper half right last-address-field">
                             <input class="inline-block postcode right half-field half-field-right" type="text" id="address_1_postal_code" name="address_1_postal_code" placeholder="Postcode" value=""/>
-                        </div>
+                        </div>-->
                     </div>    
                 <?php }; ?>
             </fieldset>
