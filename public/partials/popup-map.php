88bfdb51a64b703c99b28420ada6fe2b3e331140
Andrew van Duivenbode <andrew@creativetransmissions.com>
2017-10-27 15:55:58 +0100

    readd popup-map

---
+++ public/partials/popup-map.php
@@ -0,0 +1,21 @@
+
+<div id="address-popup" class="mfp-hide white-popup-block">
+    <h3 class="heading-popup">Please Select Your Addresses</h3>
+    <div class="row">
+        <div class="moving-from-div">
+            <input type="text" id="address_0" name="address_0" class="addresspicker columns " placeholder="MOVING FROM*"> 
+            <a href="#" class="no-address no-address-0">I can&apos;t find my address</a>
+        </div>
+        <div class="moving-to-div">
+            <input type="text" id="address_1"  name="address_1"class="addresspicker columns " placeholder="MOVING TO*">
+            <a href="#" class="no-address no-address-1">I can&apos;t find my address</a>
+        </div>
+
+    </div>   
+    <div class="moving-map-cls">
+        <div id ="map"></div>
+    </div>
+    <input type="hidden" name="distance" value=""/>
+    <input class="hours" type="hidden" name="time" value=""/>
+    <div style="clear: left; text-align: center;"><input type="button" class="tq-button" name="addresses-ok" value="Addresses Are Correct"/></div>
+</div>
