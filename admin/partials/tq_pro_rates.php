2ea26b50e3e09603d29eea1bfa36d382c41eb998
Andrew van Duivenbode <andrew@creativetransmissions.com>
2017-12-28 08:34:28 +0000

    add max distance to rates tab

--- admin/partials/tq_pro_rates.php
+++ admin/partials/tq_pro_rates.php
@@ -1,9 +1,18 @@
 <?php settings_fields( 'tq_pro_rates' ); ?>
 <?php do_settings_sections( 'tq_pro_rates' ); ?>
+<p>The prices on each row apply to the parts of the journey up to the boundary distance. For example if you enter a boundary distance of 20 and a set price of £10, the first 20 miles will be charged adt £10.</p>
+<p>Prices entered against a distance boundary of 0 will apply to all remaining miles after the highest distance boundary. For example if you have a boundary of 20 miles and a boundary of 0 miles, the first 20 miles will be charged with the 20 mile boundary rate and the remaining miles will be charged with the 0 boundary rate.</p> 
+<p>You can charge with a different set of rates depending on journey length or the vehicle or service selected by the customer. When entering a rate, use the drop down boxes to select the options that will cause the rate to be used. For example if you select Maximum Distance = 50, Vehicle = Large Van, Service = Express, the rates will only apply to journeys of up to 50 miles when the customer has selected Large Van as the vehicle and Express as the service.</p>
 <form class="box-form round tq-admin-form" id="edit_rate_form" method="post" autocomplete="false">
 	<fieldset>
 		<legend>Add a new rate by entering the details below:</legend>
 		<div class="row">
+			<div class="inline-field">
+				<label for="group">Select Max Distance</label>
+				<select name="journey_length_id" class="inline">
+					<?php $this->admin->plugin->render_journey_length_options(); ?>
+				</select>
+			</div>
 			<div class="inline-field">
 				<label for="group">Select Service</label>
 				<select name="service_id" class="inline">
@@ -19,7 +28,7 @@
 		</div>
 		<div class="row row-rates">
 			<div class="inline-field">
-				<label for="distance">Max Distance (<?php echo $this->admin->distance_unit; ?>s)</label>
+				<label for="distance">Boundary Distance (<?php echo $this->admin->distance_unit; ?>s)</label>
 				<input class="input-long" type="text" name="distance" />
 			</div>
 			<div class="inline-field">
@@ -54,6 +63,12 @@
 	<fieldset class="service-filters">
 		<legend>Filter the rates below by service or vehicle</legend>
 		<div class="inline-field">
+			<label for="group">Select Max Distance</label>
+			<select name="journey_length_id" class="inline">
+				<?php $this->admin->plugin->render_journey_length_options(); ?>
+			</select>
+		</div>
+		<div class="inline-field">
 			<label for="group">Select Service</label>
 			<select name="service_id" class="inline">
 				<?php $this->admin->plugin->render_service_options(); ?>
@@ -68,7 +83,7 @@
  	</fieldset>
 	<table id="rates_table" class="settings-table">
 		<thead>
-			<tr><th>Max Distance</th><th>Set Price</th><th>Price Per <?php echo $this->admin->distance_unit; ?></th><th>Price Per Hour</th><th class="actions"><div class="spinner"></div></tr>
+			<tr><th>Boundary Distance</th><th>Set Price</th><th>Price Per <?php echo $this->admin->distance_unit; ?></th><th>Price Per Hour</th><th class="actions"><div class="spinner"></div></tr>
 		</thead>
 		<tbody>
 		</tbody>
