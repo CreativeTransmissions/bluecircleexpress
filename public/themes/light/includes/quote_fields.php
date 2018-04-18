19053fcbcc8f6e7bd66d83f60c0ebd4ce1b95540
Andrew van Duivenbode <andrew@creativetransmissions.com>
2018-04-18 08:44:36 +0100

    form fix

--- public/themes/light/includes/quote_fields.php
+++ public/themes/light/includes/quote_fields.php
@@ -3,15 +3,14 @@
         <h2>Delivery Cost</h2>
     </div>
     <div class="transit_body">
-        <span>ESTIMATE COST</span>
-        <h3><span class="totalCost" ></span> <?php echo $this->currency_symbol; ?></h3>
+        <span>Our Quote</span>
+        <h3><?php echo $this->currency_symbol; ?><span class="totalCost" ></span></h3>
         <ul>
             <li>Distance (<?php echo $this->distance_unit; ?>s): <span id="distance"></span></li>
-            <li class="<?php echo $tax_hidden_class; ?>">Estimated Travel Time: <span id="hours"></span></li>
-            <li class="<?php echo $tax_hidden_class; ?>">Short Notice Cost: <span class="noticeCost"></span><?php echo $this->currency_symbol; ?></li>
-            <li class="field <?php echo $tax_hidden_class; ?>">Estimated Cost: <span class="basicCost"></span> <?php echo $this->currency_symbol; ?></li>
-            <li class="<?php echo $tax_hidden_class; ?>">Tax: <span class="taxCost"></span> <?php echo $this->currency_symbol; ?></li>
-            <!-- <li>Total Cost: <span class="totalCost" ></span> <?php //echo $this->currency_symbol; ?></li> -->
+            <li class="<?php echo $drive_time_hidden_class; ?>">Estimated Travel Time: <span id="hours"></span></li>
+            <li class="hidden">Short Notice Cost: <?php echo $this->currency_symbol; ?><span class="noticeCost"></span></li>
+            <li class="field">Delivery Cost: <?php echo $this->currency_symbol; ?><span class="basicCost"></span></li>
+            <li class="<?php echo $tax_hidden_class; ?>">Tax: <?php echo $this->currency_symbol; ?><span class="taxCost"></span></li>
         </ul>
 
     </div>
