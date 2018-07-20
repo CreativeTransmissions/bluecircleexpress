2fb625d3dd6de8efa88d7ddc0a32af76bb1af928
Andrew van Duivenbode <andrew@creativetransmissions.com>
2018-07-20 16:59:50 +0100

    updated validation

--- public/themes/classic/includes/customer_fields.php
+++ public/themes/classic/includes/customer_fields.php
@@ -8,24 +8,24 @@
 				<div class="tq-col48">
 					<div class="tq-row bt-flabels__wrapper">
 						<label for="first_name">First Name</label>
-						<input tabindex="1" type="text" name="first_name" id="first_name" required placeholder="First Name*">
+						<input tabindex="1" type="text" name="first_name" id="first_name" data-parsley-maxlength="45" required placeholder="First Name*">
 						<span class="bt-flabels__error-desc">Required / Invalid First Name</span>
 					</div>
 					<div class="tq-row bt-flabels__wrapper">
 						<label for="phone">Phone Number</label>
-						<input tabindex="3" type="text" data-parsley-type="number" required name="phone" class="phone" placeholder="Phone Number*">
+						<input tabindex="3" type="text" data-parsley-type="number" required name="phone" class="phone" data-parsley-maxlength="45" placeholder="Phone Number*">
 						<span class="bt-flabels__error-desc">Required / Invalid Phone Number </span>
 					</div>
 				</div>
 				<div class="tq-col48 right">
 					<div class="tq-row bt-flabels__wrapper">
 						<label for="last_name">Last Name</label>
-						<input tabindex="2" type="text" name="last_name" id="last_name" required placeholder="Last Name*">
+						<input tabindex="2" type="text" name="last_name" id="last_name" required data-parsley-maxlength="45" placeholder="Last Name*">
 						<span class="bt-flabels__error-desc">Required / Invalid Last Name </span>
 					</div>
 					<div class="tq-row bt-flabels__wrapper">
 						<label for="email">Email Address</label>
-						<input tabindex="4" type="text" name="email" id="email" data-parsley-type="email"  placeholder="Email Address*" required>
+						<input tabindex="4" type="text" name="email" id="email" data-parsley-type="email" data-parsley-maxlength="128" placeholder="Email Address*" required>
 						<span class="bt-flabels__error-desc">Required / Invalid Email</span>
 					</div>
 				</div>
