c061364468f81d910530e3869240a1982c4d7565
Andrew van Duivenbode <andrew@creativetransmissions.com>
2019-04-23 18:31:29 +0100

    update version

--- build/transitquote_pro_release4.bat
+++ build/transitquote_pro_release4.bat
@@ -1,9 +1,9 @@
 set source="C:\vagrant\www\pro\public_html\wp-content\plugins\transitquote-pro"
-set destination="C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9\"
-del "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9\*.*" /s /q
+set destination="C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0\"
+del "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0\*.*" /s /q
 
 xcopy %source% %destination% /E /y
-del "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9\*.git*" /s
-"C:\Users\Transmissions HQ\7z.exe" a  "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9.zip" "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9\*"
+del "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0\*.git*" /s
+"C:\Users\Transmissions HQ\7z.exe" a  "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0.zip" "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0\*"
 
-xcopy "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-2-9.zip" "C:\Users\Transmissions HQ\Dropbox\releases\pro\transitquote-pro.zip" 
+xcopy "C:\Users\Transmissions HQ\Documents\Code\releases\transitquote-pro-v4-3-0.zip" "C:\Users\Transmissions HQ\Dropbox\releases\pro\transitquote-pro.zip" 
