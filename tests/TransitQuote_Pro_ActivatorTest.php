3b7b78caf9caa47c50306ef7c0c4d70ee107794a
Andrew van Duivenbode <andrew@creativetransmissions.com>
2020-01-08 12:34:43 +0000

    tests for new db fields added on activation

--- tests/TransitQuote_Pro_ActivatorTest.php
+++ tests/TransitQuote_Pro_ActivatorTest.php
@@ -17,4 +17,26 @@ final class TransitQuote_Pro_ActivatorTest extends TestCase
         $success = TransitQuote_Pro_Activator::activate();
         $this->assertTrue($success);
     }
+
+ 	public function test_check_db() {
+        $public = new TransitQuote_Pro_Public('TransitQuote Pro', 'tq-pro','BC.4.3.1.3');
+
+       	$has_weight_field = $public->cdb->col_exists('jobs','weight');
+       	$has_ref_field = $public->cdb->col_exists('jobs','customer_reference');
+        $has_job_ref_field = $public->cdb->col_exists('jobs','job_ref');
+        $has_collection_date_field = $public->cdb->col_exists('journeys_locations','collection_date');
+        $has_time_type_field = $public->cdb->col_exists('journeys_locations','time_type');
+        $has_visit_type_field = $public->cdb->col_exists('journeys_locations','visit_type');
+
+        $this->assertTrue($has_weight_field);
+        $this->assertTrue($has_ref_field);
+        $this->assertTrue($has_job_ref_field);
+
+        $this->assertTrue($has_collection_date_field);
+        $this->assertTrue($has_time_type_field);
+        $this->assertTrue($has_visit_type_field);
+        
+        $public = null;
+
+    }     
 }
