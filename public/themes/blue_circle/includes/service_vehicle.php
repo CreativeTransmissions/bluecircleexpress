9ec2794b272ffb7db19496a0238833cca5bd76c1
Andrew van Duivenbode <andrew@creativetransmissions.com>
2020-01-21 11:55:52 +0000

    added missing labels

--- public/themes/blue_circle/includes/service_vehicle.php
+++ public/themes/blue_circle/includes/service_vehicle.php
@@ -76,7 +76,7 @@ if (!((empty($get_services_by_id)) && (empty($get_services_with_rates_by_id)))) 
 					<input tabindex="7" name="service_id" id="service_id" value="<?php echo($attributes['service']);?>" type="hidden">
 				<?php }else{ ?>
 					<div class="left full">
-						<span class="sub_title"><i class="icon icon-icn-service"></i><?php echo $this->view_labels['service_id']; ?></span>
+						<span class="sub_title"><i class="icon icon-icn-service"></i><?php echo $this->view_labels['service_label']; ?></span>
 						<div class="bt-flabels__wrapper select-wrap">
 							<select tabindex="7" name="service_id" id="service_id">
 							<?php $this->render_service_options_with_rates(); ?>
--- public/themes/light/includes/repeat_customer_fields.php
+++ public/themes/light/includes/repeat_customer_fields.php
@@ -5,26 +5,26 @@
     <div class="transit_body tq-form-bg-color">
         <div class="full">
             <div class="left half bt-flabels__wrapper">
-                <span class="sub_title"><i class="icon icon-icn-name"></i><?php echo $this->view_labels['first_name']; ?></span>
-                <input type="text" name="first_name" required data-parsley-maxlength="45" placeholder="Enter <?php echo $this->view_labels['first_name']; ?>" value="<?php echo $this->customer['first_name']?>">
-                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['first_name']; ?></span>
+                <span class="sub_title"><i class="icon icon-icn-name"></i><?php echo $this->view_labels['first_name_label']; ?></span>
+                <input type="text" name="first_name" required data-parsley-maxlength="45" placeholder="Enter <?php echo $this->view_labels['first_name_label']; ?>" value="<?php echo $this->customer['first_name']?>">
+                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['first_name_label']; ?></span>
             </div>
             <div class="right half bt-flabels__wrapper">
-                <span class="sub_title"><i class="icon icon-icn-name"></i><?php echo $this->view_labels['last_name']; ?></span>
-                <input type="text" name="last_name" required  data-parsley-maxlength="45" placeholder="Enter <?php echo $this->view_labels['last_name']; ?>"  value="<?php echo $this->customer['last_name']?>">
-                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['last_name']; ?></span>
+                <span class="sub_title"><i class="icon icon-icn-name"></i><?php echo $this->view_labels['last_name_label']; ?></span>
+                <input type="text" name="last_name" required  data-parsley-maxlength="45" placeholder="Enter <?php echo $this->view_labels['last_name_label']; ?>"  value="<?php echo $this->customer['last_name']?>">
+                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['last_name_label']; ?></span>
             </div>
         </div>
         <div class="full">
             <div class="left half bt-flabels__wrapper">
-                <span class="sub_title"><i class="icon icon-icn-phone-number"></i><?php echo $this->view_labels['phone']; ?></span>
-                <input type="text" data-parsley-type="number" required name="phone" class="phone" data-parsley-maxlength="45" placeholder="Enter your <?php echo $this->view_labels['phone']; ?>"  value="<?php echo $this->customer['phone']?>">
-                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['phone']; ?></span>
+                <span class="sub_title"><i class="icon icon-icn-phone-number"></i><?php echo $this->view_labels['phone_label']; ?></span>
+                <input type="text" data-parsley-type="number" required name="phone" class="phone" data-parsley-maxlength="45" placeholder="Enter your <?php echo $this->view_labels['phone_label']; ?>"  value="<?php echo $this->customer['phone']?>">
+                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['phone_label']; ?></span>
             </div>
             <div class="right half bt-flabels__wrapper">
-                <span class="sub_title"><i class="icon icon-icn-email"></i><?php echo $this->view_labels['email']; ?></span>
-                <input type="text" data-parsley-type="email" name="email" required data-parsley-maxlength="128" placeholder="Enter your <?php echo $this->view_labels['email']; ?>"  value="<?php echo $this->customer['email']?>">
-                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['email']; ?></span>
+                <span class="sub_title"><i class="icon icon-icn-email"></i><?php echo $this->view_labels['email_label']; ?></span>
+                <input type="text" data-parsley-type="email" name="email" required data-parsley-maxlength="128" placeholder="Enter your <?php echo $this->view_labels['email_label']; ?>"  value="<?php echo $this->customer['email']?>">
+                <span class="bt-flabels__error-desc">Required / Invalid <?php echo $this->view_labels['email_label']; ?></span>
             </div>
         </div>
         <div class="full bt-flabels__wrapper">
